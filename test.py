3198d4873d897ce4c18e98899d84c93508cef94b
Donghwa Shin <68676833+dhshin04@users.noreply.github.com>
2024-08-18 17:47:55 -0400

    Adjusted TODO comment

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 # from stock_market_predictor.predict import function_name
 
-# TODO: Implement code that tests stock market predictor on real-time data
+# TODO: Implement code that tests stock market predictor on real-time data (using Stock API)
